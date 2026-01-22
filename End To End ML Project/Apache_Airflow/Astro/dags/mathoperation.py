53ce3ef7414f8f870e0298bfe46ab19730d0c118
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-01-22 06:42:55 +0600

    upd

--- End To End ML Project/Apache_Airflow/Astro/dags/mathoperation.py
+++ End To End ML Project/Apache_Airflow/Astro/dags/mathoperation.py
@@ -3,22 +3,34 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime 
 
 
-def initial_number():
+def initial_number(**context):
+    context["ti"].xcom_push(key="current_value",value=10)
     print("Initial Number")
 
-def add():
+def add(**context):
+    current_value=context["ti"].xcom_pull(key="current_value",task_ids="initial_number")
+    new_value=current_value+5
+    context["ti"].xcom_push(key="new_value",value=new_value)
     print("add 5")
 
-def multiply():
+def multiply(**context):
+    current_value=context["ti"].xcom_pull(key="current_value",task_ids="add")
+    new_value=current_value*3
+    context["ti"].xcom_push(key="new_value",value=new_value)
     print("multiple 5")
 
-def subtract():
+def subtract(**context):
+    current_value=context["ti"].xcom_pull(key="current_value",task_ids="multiply")
+    new_value=current_value-5
+    context["ti"].xcom_push(key="new_value",value=new_value)
     print("subtract 3")
 
-def compute():
+def compute(**context):
+    current_value=context["ti"].xcom_pull(key="current_value",task_ids="substract")
+    new_value=current_value+5
     print("compute")
 
-with DAG("math_operations",start_date=datetime(2025,10,10),schedule="@daily") as dag:
+with DAG("math_operations",start_date=datetime(2025,10,10),schedule="@daily",catchup=False) as dag:
     initial=PythonOperator(task_id="initial_number",python_callable=initial_number)
     ad=PythonOperator(task_id="add",python_callable=add)
     mul=PythonOperator(task_id="multiply",python_callable=multiply)
