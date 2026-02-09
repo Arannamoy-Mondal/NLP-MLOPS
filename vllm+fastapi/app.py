fda4cbe63c204965475027573c7155311f2427e6
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-02-09 11:24:56 +0600

    upd:AlexNet.ipynb and vllm+fastapi/app.py

--- vllm+fastapi/app.py
+++ vllm+fastapi/app.py
@@ -1,3 +1,21 @@
 from fastapi import FastAPI
-from
+import httpx
 app=FastAPI()
+
+@app.post("/:question:str")
+async def questiion_answer(question:str):
+    print(question)
+    async with httpx.AsyncClient() as client:
+        response = (await client.post(
+            "http://localhost:8000/v1/chat/completions",
+            json={
+                "model": "microsoft/Phi-4-mini-instruct",
+                "messages": [{"role": "user", "content": question}]
+            }
+        )).json()
+        print(response['id'])
+        print(response['choices'][0]['message']['content'],type(response['choices'][0]['message']))
+        return response['choices'][0]['message']['content']
+
+
+    
